4d8b4c111bc545e6e6838634b59c37570dd0a4eb
NguyenDuyKhai2 <khainguyenduy5@gmail.com>
2026-03-30 01:19:52 +0700

    fix

--- config/aws-config.js
+++ config/aws-config.js
@@ -5,8 +5,8 @@ const { DynamoDBDocument } = require("@aws-sdk/lib-dynamodb");
 const info = {
     region : "ap-southeast-1",
     credentials : {
-        accessKeyId : "AKIAQQD2ETBBYAT3TGEJ",
-        secretAccessKey : "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",
+        accessKeyId : "",
+        secretAccessKey : "",
     },
 };
 
